934e51b88b2dd42cb70407f6ccaa770f9cb947f3
Avinash Malhotra <avimalhotra505@gmail.com>
2021-07-11 10:53:04 +0530

    express get posy

--- src/server.js
+++ src/server.js
@@ -6,13 +6,17 @@ const app=express();
 app.use(express.static(path.resolve('src/public')));
 
 app.use((req,res,next)=>{
-    console.log(`login at ${Date.now()} `);
+    console.log(`login at ${Date.now()}`);
     next();
 });
 
-
 app.get('/',(req,res)=>{
-    res.send("hello express");
+    //res.send(req.url);
+    res.send(req.query);
+});
+
+app.get('/app',(req,res)=>{
+    res.redirect('/app.html')
 });
 app.get('/login',(req,res)=>{
     res.send("please login");
@@ -21,15 +25,21 @@ app.get('/getdata',(req,res)=>{
 
     let name=req.query.name;
     let age=req.query.age;
-    
 
-    //res.send(req.query);
-    res.json({data:req.query});
+    res.status(200).send(req.query);
+    //res.json({data:req.query});
 });
 app.post('/postdata',(req,res)=>{
     res.send("data posted");
 });
 
+app.get('/search',(req,res)=>{
+    res.status(200).send(`you have searched  <b> ${req.query.q} </b>`);
+});
+
+app.get('/cars/:brand/:car/:variant',(req,res)=>{
+    res.status(200).send(req.params);
+});
 
 /* Wildcard Handler */ 
 app.get('/**',(req,res)=>{
